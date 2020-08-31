a40118907c69b0c3cae78a7f33a3177a7cfe6fd2
Gus Ludwig <gustavo.ludwig@gmail.com>
2020-08-31 21:38:16 +0100

    added endpoint for generating random face definition

--- src/main/java/com/gusludwig/geomorphichextiles/service/FaceService.java
+++ src/main/java/com/gusludwig/geomorphichextiles/service/FaceService.java
@@ -4,11 +4,15 @@ import com.gusludwig.geomorphichextiles.persistence.model.ContactPoint;
 import com.gusludwig.geomorphichextiles.persistence.model.Face;
 import com.gusludwig.geomorphichextiles.service.generic.CrudService;
 
+import java.util.Optional;
+
 public interface FaceService extends CrudService<Face> {
 
-    Face create(ContactPoint[] contactPoints);
+    Optional<Face> create(ContactPoint[] contactPoints);
 
-    Face update(String id, ContactPoint[] contactPoints);
+    Optional<Face> update(String id, ContactPoint[] contactPoints);
 
     boolean validateFace(Face face);
+
+    Optional<Face> createOneRandomFace();
 }
