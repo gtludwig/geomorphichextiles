26d7ddd57f23e85bcbd17edc152992cb9a460150
Gus Ludwig <gustavo.ludwig@gmail.com>
2020-08-31 21:32:02 +0100

    added swagger api documentation support

--- src/main/java/com/gusludwig/geomorphichextiles/GeomorphichextilesApplication.java
+++ src/main/java/com/gusludwig/geomorphichextiles/GeomorphichextilesApplication.java
@@ -2,12 +2,39 @@ package com.gusludwig.geomorphichextiles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class GeomorphichextilesApplication {
 
+    private static final String MESSAGE_SOURCE = "i18n/messages";
+
+    @Bean(name = "messageSource")
+    @Description("Spring message resolver")
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename(MESSAGE_SOURCE);
+        messageSource.setFallbackToSystemLocale(false);
+        messageSource.setCacheSeconds(0);
+        messageSource.setDefaultEncoding("utf-8");
+        return messageSource;
+    }
+
+    @Bean(name = "localeResolver")
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(new Locale("en","IE"));
+        return sessionLocaleResolver;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(GeomorphichextilesApplication.class, args);
     }
-
 }
---
+++ src/test/java/com/gusludwig/geomorphichextiles/service/impl/FaceServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.gusludwig.geomorphichextiles.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FaceServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void validateFace() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
