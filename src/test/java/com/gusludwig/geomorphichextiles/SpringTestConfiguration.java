d99ed4bfb21193b96194d0ff88b1d663004d426d
Gus Ludwig <gustavo.ludwig@gmail.com>
2020-09-06 18:32:58 +0100

    initial test coverage

---
+++ src/test/java/com/gusludwig/geomorphichextiles/SpringTestConfiguration.java
@@ -0,0 +1,7 @@
+package com.gusludwig.geomorphichextiles;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringTestConfiguration {
+}
--- src/test/java/com/gusludwig/geomorphichextiles/service/impl/FaceServiceImplTest.java
+++ src/test/java/com/gusludwig/geomorphichextiles/service/impl/FaceServiceImplTest.java
@@ -1,42 +1,107 @@
 package com.gusludwig.geomorphichextiles.service.impl;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.gusludwig.geomorphichextiles.SpringTestConfiguration;
+import com.gusludwig.geomorphichextiles.persistence.dao.FaceRepository;
+import com.gusludwig.geomorphichextiles.persistence.model.ContactPointType;
+import com.gusludwig.geomorphichextiles.persistence.model.Face;
+import com.gusludwig.geomorphichextiles.service.FaceService;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = { SpringTestConfiguration.class })
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class FaceServiceImplTest {
 
-    @BeforeEach
+//    @Mock
+//    private FaceRepository faceRepository;
+
+//    @InjectMocks
+//    private FaceService faceService;
+
+    private Face face;
+
+    private Random random;
+
+    @BeforeAll
     void setUp() {
+        MockitoAnnotations.initMocks(this);
+        random = new Random();
+
+        buildRandomFace();
     }
 
-    @Test
-    void create() {
+    private Face buildRandomFace() {
+//        random = new Random();
+
+        face = new Face();
+
+        face.setPointZero(getRandomContactPointType());
+        face.setPointOne(getRandomContactPointType());
+        face.setPointTwo(getRandomContactPointType());
+        face.setPointThree(getRandomContactPointType());
+        face.setPointFour(getRandomContactPointType());
+        face.setPointFive(getRandomContactPointType());
+        face.setPointSix(getRandomContactPointType());
+        face.setPointSeven(getRandomContactPointType());
+        face.setPointEight(getRandomContactPointType());
+        face.setPointNine(getRandomContactPointType());
+        face.setPointTen(getRandomContactPointType());
+        face.setPointEleven(getRandomContactPointType());
+        face.setPointTwelve(getRandomContactPointType());
+
+        return face;
     }
 
-    @Test
-    void validateFace() {
+    private ContactPointType getRandomContactPointType() {
+        return ContactPointType.values()[random.nextInt(ContactPointType.values().length)];
     }
 
-    @Test
-    void update() {
-    }
+//    @Test
+//    void create() {
+//    }
 
     @Test
-    void findById() {
+    void assertBlahTrue() {
+        Assert.assertTrue(true);
     }
 
-    @Test
-    void save() {
-    }
+//    @Test
+//    void validateFace() {
+//        face = buildRandomFace();
+//        Mockito.when(faceService.validateFace(face)).thenReturn(true);
+//        Assert.assertTrue(faceService.validateFace(face));
+//    }
 
-    @Test
-    void remove() {
-    }
-
-    @Test
-    void findAll() {
-    }
+//    @Test
+//    void update() {
+//    }
+//
+//    @Test
+//    void findById() {
+//    }
+//
+//    @Test
+//    void save() {
+//    }
+//
+//    @Test
+//    void remove() {
+//    }
+//
+//    @Test
+//    void findAll() {
+//    }
 }
